9f0e9d7256d2caa05a0b4f4137b33e4270176a58
Josephdada01 <ridwandada500@gmail.com>
2024-04-05 13:53:45 +0100

    done with designing the adminsite

--- library/libraryapp/admin.py
+++ library/libraryapp/admin.py
@@ -9,22 +9,35 @@ from libraryapp.models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BookInline(admin.TabularInline):
+    """Inline listing for books inside Author"""
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
+    inlines = [BookInline]
+
 admin.site.register(Author, AuthorAdmin)
 
+
+class BooksInstanceInline(admin.TabularInline):
+    """inline listing for bookinstance inside Bookadmin"""
+    model = BookInstance
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
+    inlines = [BooksInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
-
+    list_display = ('book', 'status', 'due_back', 'id')
+    
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
