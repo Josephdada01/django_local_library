fe0c3184d0fe4a6004b7829131c5ade6c31950cd
Josephdada01 <ridwandada500@gmail.com>
2024-05-05 15:40:03 +0100

    made changes

--- library/library/settings.py
+++ library/library/settings.py
@@ -36,7 +36,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Josephdada01.pythonanywhere.com', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://Josephdada01.pythonanywhere.com']
+
 
 
 # Application definition
